4bda014a1e0426de305ff502b44bc920d4513fcc
jsrimr <jsrimr@naver.com>
2020-05-17 23:35:47 +0900

    DB등록

--- blog/settings.py
+++ blog/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblog',  #mysql
+        'USER': 'root', #root
+        'PASSWORD': '1234qwer!@', #1234
+        'HOST': '15.165.160.119',
     }
 }
 
--- user/forms.py
+++ user/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label = "Kullanıcı Adı")
-    password = forms.CharField(label = "Parola",widget = forms.PasswordInput)
+    username = forms.CharField(label = "사용자 이름")
+    password = forms.CharField(label = "비밀번호",widget = forms.PasswordInput)
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length = 50,label = "Kullanıcı Adı")
-    password = forms.CharField(max_length=20,label = "Parola",widget = forms.PasswordInput)
-    confirm = forms.CharField(max_length=20,label ="Parolayı Doğrula",widget = forms.PasswordInput)
+    username = forms.CharField(max_length = 50,label = "사용자 이름")
+    password = forms.CharField(max_length=20,label = "비밀번호",widget = forms.PasswordInput)
+    confirm = forms.CharField(max_length=20,label ="비밀번호 확인",widget = forms.PasswordInput)
     
     def clean(self):
         username = self.cleaned_data.get("username")
@@ -16,7 +16,7 @@ class RegisterForm(forms.Form):
         confirm = self.cleaned_data.get("confirm")
 
         if password and confirm and password != confirm:
-            raise forms.ValidationError("Parolalar Eşleşmiyor")
+            raise forms.ValidationError("비밀번호가 일치하지 않습니다")
 
         values = {
             "username" : username,
